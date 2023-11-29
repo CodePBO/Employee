79368d3bf5a5aa133877492dd76e00aad8ad8a22
AnugrahPanjiPradipa <panjipradipa714@gmail.com>
2023-11-29 12:15:39 +0700

    First

---
+++ src/com/tubes/program/Konfig.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tubes.program;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author User
+ */
+public class Konfig {
+    
+    private static Connection MySQLConfig;
+    
+    public static Connection configDB()throws SQLException {
+        try{
+            String url = "jdbc.mysql://localhost:3306/employee";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+        }catch(SQLException e){
+            System.out.println("Koneksi ke Database Gagal " + e.getMessage());
+        }
+        return MySQLConfig;
+    }
+    
+}
